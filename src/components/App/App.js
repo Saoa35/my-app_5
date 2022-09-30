8ea56315a4fb9ad3a6a860957f5ad8d01880597c
Saoa35 <saoamah@gmail.com>
2022-09-30 20:50:58 +0200

    installed uuid library

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { v4 } from 'uuid';
 import List1 from '../List1/List1';
 import List2 from '../List2/List2';
-
 import './App.css';
 
 export default class App extends React.Component {
@@ -15,16 +15,59 @@ export default class App extends React.Component {
     super(props);
 
     for(let i = 0; i < 10; i++) {
-
+      this.state.items.push({
+        item: faker.internet.userName(),
+        id: v4()
+      })
     }
+  }
+
+  changeListType() {
+    this.setState({
+      flag: !this.state.flag
+    })
+  }
+
+  addNewItemToStart() {
+    const newItem = faker.internet.userName();
+
+    this.setState({
+      items: [
+        {
+          item:newItem,
+          id: v4()
+        },
+        ...this.state.items,
+      ]
+    })
+  }
+
+  addNewItem() {
+    const newItem = faker.internet.userName();
+
+    this.setState({
+      items: [
+        ...this.state.items,
+        {
+          item:newItem,
+          id: v4()
+        },
+      ]
+    })
   }
   
   render() {
 
     return (
       <div className="App">
-        <List1/>
-        <List2/>
+        
+        <button onClick={this.changeListType.bind(this)}>Change List Type</button>
+
+        <button onClick={this.addNewItem.bind(this)}>Add to the end</button>
+        <button onClick={this.addNewItemToStart.bind(this)}>Add to the end</button>
+
+        {this.state.flag ? <List1 items={this.state.items}/> : <List2 items={this.state.items}/>}
+
       </div>
     );
   }
